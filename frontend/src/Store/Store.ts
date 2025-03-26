bc60c3b0060735103e88d99c440aaff93cd64ecb
Shreyansh Kabra <kabras@usc.edu>
2025-03-26 12:52:43 -0700

    Adding redux dev tools extension changes

--- frontend/src/Store/Store.ts
+++ frontend/src/Store/Store.ts
@@ -2,16 +2,18 @@ import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./Reducer";
 import { RootSaga } from "./Saga";
-// import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const sagaMiddleware = createSagaMiddleware();
 // const store = createStore(
 //   rootReducer,
-//   composeWithDevTools(applyMiddleware(sagaMiddleware))
+//   applyMiddleware(sagaMiddleware),
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 // );
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(sagaMiddleware)
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 );
 
 sagaMiddleware.run(RootSaga);
