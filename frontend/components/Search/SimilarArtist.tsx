ec0f1826d070c3fdf892e1813a24eee8ee4aa794
Shreyansh Kabra <kabras@usc.edu>
2025-04-01 00:51:39 -0700

    Added Similar Artists heading

--- frontend/components/Search/SimilarArtist.tsx
+++ frontend/components/Search/SimilarArtist.tsx
@@ -38,63 +38,68 @@ const SimilarArtist: React.FC<SimilarArtistProps> = ({
   return (
     <>
       <Container>
-        {user && Object.keys(user).length > 0 && (
-          <Row className="d-flex-similar overflow-auto">
-            {similar_artists &&
-              activeTab == "artist-info" &&
-              similar_artists.map((artist: any) => {
-                const isStarred =
-                  starredArtists.find((id) => id === artist.id) !== undefined;
-                return (
-                  <Col
-                    key={artist.id}
-                    className="d-flex-similar justify-content-center"
-                  >
-                    <Card
-                      className={`${
-                        selectedArtist === artist.id ? "selected" : ""
-                      } custom-card-similar`}
-                      onClick={() => {
-                        handleArtistClick(artist.id);
-                      }}
+        {user &&
+          Object.keys(user).length > 0 &&
+          activeTab === "artist-info" &&
+          similar_artists &&
+          similar_artists.length > 0 && (
+            <>
+              <h3>Similar Artists</h3>
+              <Row className="d-flex-similar overflow-auto">
+                {similar_artists.map((artist: any) => {
+                  const isStarred =
+                    starredArtists.find((id) => id === artist.id) !== undefined;
+                  return (
+                    <Col
+                      key={artist.id}
+                      className="d-flex-similar justify-content-center"
                     >
-                      {Object.keys(user).length > 0 && (
-                        <div
-                          className="star-icon-wrapper"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            handleStarClick(artist?.id, isStarred);
-                          }}
-                        >
-                          <FontAwesomeIcon
-                            icon={isStarred ? solidStar : regularStar}
-                            style={{
-                              color: isStarred ? "gold" : "white",
-                              cursor: "pointer",
-                              fontSize: "20px",
-                            }}
-                          />
-                        </div>
-                      )}
-                      <Card.Img
-                        variant="top"
-                        src={artist.thumbnail || artsy_logo}
-                        alt={artist.name}
-                        className="fixed-img"
-                      />
-                      <Card.Body
-                        className="text-white custom-card-similar-body"
-                        // style={{ backgroundColor: "#205375" }}
+                      <Card
+                        className={`${
+                          selectedArtist === artist.id ? "selected" : ""
+                        } custom-card-similar`}
+                        onClick={() => {
+                          handleArtistClick(artist.id);
+                        }}
                       >
-                        {/* <Card.Text className="custom-card-similar-body">{artist.name}</Card.Text> */}
-                        <p className="artist-name">{artist.name}</p>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                );
-              })}
-          </Row>
-        )}
+                        {Object.keys(user).length > 0 && (
+                          <div
+                            className="star-icon-wrapper"
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              handleStarClick(artist?.id, isStarred);
+                            }}
+                          >
+                            <FontAwesomeIcon
+                              icon={isStarred ? solidStar : regularStar}
+                              style={{
+                                color: isStarred ? "gold" : "white",
+                                cursor: "pointer",
+                                fontSize: "20px",
+                              }}
+                            />
+                          </div>
+                        )}
+                        <Card.Img
+                          variant="top"
+                          src={artist.thumbnail || artsy_logo}
+                          alt={artist.name}
+                          className="fixed-img"
+                        />
+                        <Card.Body
+                          className="text-white custom-card-similar-body"
+                          // style={{ backgroundColor: "#205375" }}
+                        >
+                          {/* <Card.Text className="custom-card-similar-body">{artist.name}</Card.Text> */}
+                          <p className="artist-name">{artist.name}</p>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  );
+                })}
+              </Row>
+            </>
+          )}
       </Container>
     </>
   );
